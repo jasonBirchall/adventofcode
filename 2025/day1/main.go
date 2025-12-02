f0e4173e1ff04ee30323a3e4fb0751698515b54d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2025-12-02 17:17:41 +0000

    ✨ Add day 1 part 2

--- 2025/day1/main.go
+++ 2025/day1/main.go
@@ -13,9 +13,10 @@ var input string
 
 func main() {
 	println("Part 1:", part1())
+	println("Part 2:", part2())
 }
 
-func rotateDial(pos int, direction, distance string) int {
+func rotateDial(pos int, direction, distance string) (int, int) {
 	dialSize := 100
 	if direction != "L" && direction != "R" {
 		fmt.Errorf("invalid direction %q in %q", direction, distance)
@@ -24,10 +25,30 @@ func rotateDial(pos int, direction, distance string) int {
 	newDistance, err := strconv.Atoi(distance)
 	if err != nil {
 		fmt.Errorf("invalid distance %q in %q: %v", direction, distance, err)
-		return 100
 	}
-	newDistance = newDistance % dialSize
 
+	hits := 0
+	if newDistance > 0 {
+		if pos == 0 {
+			// it has started at zero
+			hits = newDistance / dialSize
+		} else {
+			var firstHit int
+			switch direction {
+			case "L":
+				firstHit = pos
+			case "R":
+				firstHit = dialSize - pos
+			}
+
+			if newDistance >= firstHit {
+				hits = 1 + (newDistance-firstHit)/dialSize
+			}
+		}
+	}
+
+	newDistance = newDistance % dialSize
+	println("Rotating from position", pos, "to the", direction, "by", newDistance)
 	switch direction {
 	case "L":
 		pos = (pos - newDistance + dialSize) % dialSize
@@ -35,7 +56,7 @@ func rotateDial(pos int, direction, distance string) int {
 		pos = (pos + newDistance) % dialSize
 	}
 
-	return pos
+	return pos, hits
 }
 
 func part1() int {
@@ -50,11 +71,30 @@ func part1() int {
 		direction := inst[:1]
 		distance := inst[1:]
 
-		position = rotateDial(position, direction, distance)
+		position, _ = rotateDial(position, direction, distance)
 		if position == 0 {
 			numberOfZeros++
 		}
 	}
 
 	return numberOfZeros
+}
+
+func part2() int {
+	position := 50
+	numberOfClicks := 0
+
+	instructions := strings.Split(input, "\n")
+	for _, inst := range instructions {
+		if inst == "" {
+			continue
+		}
+		direction := inst[:1]
+		distance := inst[1:]
+
+		var hits int
+		position, hits = rotateDial(position, direction, distance)
+		numberOfClicks += hits
+	}
+	return numberOfClicks
 }
